46458cd17b045f6cf468dee721f5ed176d0ffc46
chiefgunner <raul07sd@163.com>
2020-06-18 15:55:34 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+//
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 接口基本设置
+// baseUrl 根据前端跨域做调整
+axios.defaults.baseUrl = '/api'
+axios.defaults.timeout = 7000
+
+// 接口拦截 interceptors
+axios.interceptors.response.use(function (response) {
+  let res = response.data
+  if (res.status === 0) {
+    return res.data
+  } else if (res.status === 10) {
+    window.location.href = '/'
+  } else {
+    alert(res.msg)
+  }
+})
+//
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
