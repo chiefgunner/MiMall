82bfea4bbc4abab8f3acb79929b485d7ee3bb67a
chiefgunner <raul07sd@163.com>
2020-06-27 18:25:29 +0800

    order-header

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>order-header</div>
+  <div class="order-header">
+    <div class="container clearfix">
+      <div class="header-logo">
+        <a href="/index"></a>
+      </div>
+      <div class="title">
+        <h2>{{title}}<slot name="tip"></slot></h2>
+      </div>
+      <div class="username">jack</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'order-header',
+  props: {
+    title: String
+  },
   data () {
     return {
 
@@ -13,6 +26,41 @@ export default {
 }
 </script>
 
-<style lang="" scoped>
-
+<style lang="scss">
+@import '@/assets/scss/mixin';
+.order-header{
+  height: 112px;
+  padding:30px;
+  .container{
+    .header-logo{
+      float: left;
+    }
+    .title,.username{
+      display:inline-block;
+      height:55px;
+      line-height:55px;
+    }
+    .title{
+      float:left;
+      margin-left:54px;
+      h2{
+        font-size:28px;
+        color:#333333;
+      }
+      span{
+        font-size:14px;
+        margin-left:17px;
+        color:#999999;
+        font-weight:200;
+      }
+    }
+    .username{
+      float:right;
+      a{
+        color:#666666;
+        font-size:16px;
+      }
+    }
+  }
+}
 </style>
