bea61d2147af90129267a7ab55c303ed0a641f07
chiefgunner <raul07sd@163.com>
2020-07-06 18:51:59 +0800

    orderlist data rendering

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,11 +1,53 @@
 <template>
-  <div>
-
-     <order-header title="订单列表">
+  <div class="order-list">
+    <order-header title="订单列表">
       <template v-slot:tip>
         <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
       </template>
     </order-header>
+    <div class="wrapper">
+      <div class="container">
+        <div class="order-box">
+          <div class="order" v-for="(order,index) in list" :key="index">
+            <div class="order-title">
+              <div class="item-info fl">
+                {{order.createTime}}
+                <span>|</span>
+                {{order.receiverName}}
+                <span>|</span>
+                订单号：{{order.orderNo}}
+                <span>|</span>
+                {{order.paymentTypeDesc}}
+              </div>
+              <div class="item-money fr">
+                <span>应付金额：</span>
+                <span class="money">{{order.payment}}</span>
+                <span>元</span>
+              </div>
+            </div>
+            <div class="order-content clearfix">
+              <div class="good-box fl">
+                <div class="good-list" v-for="(item,i) in order.orderItemVoList" :key="i">
+                  <div class="good-img">
+                    <img v-lazy="item.productImage" alt="">
+                  </div>
+                  <div class="good-name">
+                    <div class="p-name">{{item.productName}}</div>
+                    <div class="p-money">{{item.currentUnitPrice +' X '+ item.quantity}} 元</div>
+                  </div>
+                </div>
+              </div>
+              <div class="good-state fr" v-if="order.status==10">
+                <router-link :to="{path:'/order/pay',query:{orderNo:order.orderNo}}">{{order.statusDesc}}</router-link>
+              </div>
+              <div class="good-state fr" v-else>
+                <a href="javascript:;">{{order.statusDesc}}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,12 +60,113 @@ export default {
   },
   data () {
     return {
-
+      list: []// 订单列表
+    }
+  },
+  mounted () {
+    this.getList()
+  },
+  methods: {
+    getList () {
+      this.axios.get('/orders').then(res => {
+        this.list = res.list
+      })
+    },
+    goPay (orderNo) {
+      // 三种路由跳转方式
+      // this.$router.push('/order/pay?orderNo='+orderNo)
+      /* this.$router.push({
+          name:'order-pay',
+          query:{
+            orderNo
+          }
+        }) */
+      this.$router.push({
+        path: '/order/pay',
+        query: {
+          orderNo
+        }
+      })
     }
   }
 }
 </script>
-
-<style lang="" scoped>
-
+<style lang="scss">
+  @import '@/assets/scss/config.scss';
+  @import '@/assets/scss/mixin.scss';
+  .order-list{
+    .wrapper{
+      background-color:$colorJ;
+      padding-top:33px;
+      padding-bottom:110px;
+      .order-box{
+        .order{
+          @include border();
+          background-color:$colorG;
+          margin-bottom:31px;
+          &:last-child{
+            margin-bottom:0;
+          }
+          .order-title{
+            @include height(74px);
+            background-color:$colorK;
+            padding:0 43px;
+            font-size:16px;
+            color:$colorC;
+            .item-info{
+              span{
+                margin:0 9px;
+              }
+            }
+            .money{
+              font-size:26px;
+              color:$colorB;
+            }
+          }
+          .order-content{
+            padding:0 43px;
+            .good-box{
+              width:88%;
+              .good-list{
+                display: flex;
+                align-items: center;
+                height:145px;
+                .good-img{
+                  width:112px;
+                  img{
+                    width:100%;
+                  }
+                }
+                .good-name{
+                  font-size:20px;
+                  color:$colorB;
+                }
+              }
+            }
+            .good-state{
+              @include height(145px);
+              font-size: 20px;
+              color:$colorA;
+              a{
+                color:$colorA;
+              }
+            }
+          }
+        }
+        .pagination{
+          text-align:right;
+        }
+        .el-pagination.is-background .el-pager li:not(.disabled).active{
+          background-color: #FF6600;
+        }
+        .el-button--primary{
+          background-color: #FF6600;
+          border-color: #FF6600;
+        }
+        .load-more,.scroll-more{
+          text-align:center;
+        }
+      }
+    }
+  }
 </style>
