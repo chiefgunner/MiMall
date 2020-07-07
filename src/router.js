f50a4fc5183c992270f81309ed87dde211963187
chiefgunner <raul07sd@163.com>
2020-07-08 01:08:46 +0800

    lazy load routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home'
-import Login from '@/views/Login'
+// import Login from '@/views/Login'
 import Index from './views/Index'
-import Product from './views/Product'
-import Detail from './views/Detail'
+// import Product from './views/Product'
+// import Detail from './views/Detail'
 import Cart from './views/Cart'
 import Order from './views/Order'
 import OrderList from './views/OrderList'
@@ -32,19 +32,19 @@ export default new Router({
         {
           path: '/product/:id',
           name: 'product',
-          component: Product
+          component: resolve => require(['./views/Product.vue'], resolve)
         },
         {
           path: '/detail/:id',
           name: 'detail',
-          component: Detail
+          component: resolve => require(['./views/Detail.vue'], resolve)
         }
       ]
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: resolve => require(['./views/Login.vue'], resolve)
     },
     {
       path: '/cart',
