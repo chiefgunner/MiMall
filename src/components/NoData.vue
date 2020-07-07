b4087517ef0fcb42da15eb31f7f4f9809889864e
chiefgunner <raul07sd@163.com>
2020-07-07 16:57:40 +0800

    nodata

---
+++ src/components/NoData.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="no-data">
+    <img src="/imgs/icon-no-data.png" alt="">
+    <p>暂无订单记录...</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'no-data',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+  .no-data{
+    text-align: center;
+    font-size: 20px;
+    color:#999999;
+    margin:50px 0;
+    img{
+      width: 260px;
+      height: 184px;
+      margin-bottom:30px;
+    }
+  }
+</style>
